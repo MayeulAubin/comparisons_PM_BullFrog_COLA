c75ecf7f89a7634db0907a5a3abfdc1a13fab8d7
Mayeul Aubin <mayeul.aubin@iap.fr>
2025-01-15 13:49:42 +0100

    latest

--- submit_comparison.sh
+++ submit_comparison.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#SBATCH --job-name=linN768z49
-#SBATCH -o /home/aubin/data/BullFrog/comparison_article/logs/linN768z49.out
-#SBATCH -e /home/aubin/data/BullFrog/comparison_article/logs/linN768z49.err
+#SBATCH --job-name=logN768z49
+#SBATCH -o /home/aubin/data/BullFrog/comparison_article/logs/logN768z49.out
+#SBATCH -e /home/aubin/data/BullFrog/comparison_article/logs/logN768z49.err
 #SBATCH -N 1                                    # Number of nodes (value or min-max)
 #SBATCH -n 64                                   # The number of tasks (i.e. cores) per node
-#SBATCH --time=0:20:00                         # Runtime in D-HH:MM, min 10 minutes
+#SBATCH --time=1-00:00:00                         # Runtime in D-HH:MM, min 10 minutes
 
 ##SBATCH --exclusive                            # Exclusive node usage
 ##SBATCH --mem=64G                              # Memory pool for all cores (see also --mem-per-cpu)
@@ -21,7 +21,7 @@ export OMP_NUM_THREADS=64
 python3 "/home/aubin/BullFrog/comparisons_PM_BullFrog_COLA/compare_PM_COLA_BullFrog_parser.py" \
     --workdir /home/aubin/data/BullFrog/comparison_article/params/ \
     --simdir_root /home/aubin/data/BullFrog/comparison_article/sims/ \
-    --name linN768z49 \
+    --name logN768z49 \
     -L 2000 \
     --size 384 \
     --Np 384 \
@@ -31,6 +31,6 @@ python3 "/home/aubin/BullFrog/comparisons_PM_BullFrog_COLA/compare_PM_COLA_BullF
     --verbosity 1 \
     --force True \
     -ts 0 \
-    -bf 50 \
+    -pm 1000 \
 
 exit 0
